4655387566d3160bfb26782beecd8778c7c14017
allen-lhj <liu772636821@163.com>
2022-05-11 18:56:58 +0800

    upate

--- vite-cdn/src/main.js
+++ vite-cdn/src/main.js
@@ -1,41 +1,64 @@
-const { createApp } = Vue;
 
-const app = Vue.createApp({})
-/**
- * 实例上暴露了很多方法
- * component 注册组件
- * directive：注册指令
- * filter：注册过滤器
- * use： 使用插件
- * 大多数这样的方法都会返回createApp创建出来的应用实例，目的是允许链式调用
- * 
- * 
-*/
-// 返回原本的应用实例
-const app2 = app.component('MyTitle', {
-  data() {
-    return {
-      title: 'I love Vue'
+ /**
+  * 应用实例
+  * 
+  * createApp -> 创建App -> 返回一个应用实例
+  *  */ 
+
+
+  const app = Vue.createApp();
+
+  app.component('MyTitle', {
+    data () {
+      return {
+        title: 'I LOVE VUE'
+      }
+    },
+    template: `<h1 v-to-lower-case>{{ title }}</h1>`
+  }).directive('toLowerCase', {
+    mounted(el) {
+      el.addEventListener('click', function () {
+        this.innerText = this.innerText.toLowerCase();
+      }, false)
     }
-  },
-  template: `<h1 v-to-lower-case>{{ title }}</h1>`
-}).directive('toLowerCase', {
-  mounted (el) {
-    el.addEventListener('click', function() {
-      this.innerText = this.innerText.toLowerCase()
-    }, false)
+  })
+  const vm = app.mount('#app');
+  console.log(vm)
+  /**
+   * 根组件的本质就是一个对象 {}
+   * createApp执行的时候需要一个根组件 createApp({})
+   * 根组件是Vue渲染的起点，一堆组件总要有一个在外面的包裹的
+   * 
+   * 
+   * 根元素是一个HTML元素
+   * createApp执行创建Vue应用实例时，需要一个HTML根元素
+   * mount 方法返回的是根组件的实例
+   */
+  
+  /**
+   * 每个组件都有自己的组件实例
+   * 一个应用中所有的组件都共享一个应用实例
+   * 无论是根组件还是应用组件内的其他的组件，
+   * 配置选项，组件行为都是一样的
+   */
+  
+  const App = {
+    components: {
+      /** title author content */
+      
+    },
+    data() {
+      return {
+        title: 'This is a TITLE',
+        authorL: 'XiaoLiu',
+        content: 'This is a CONTENT'
+      }
+    },
+    template: `
+      <div>
+        <my-title>{{ title }}</my-title>
+        <my-author>{{ author }}</my-author>
+        <my-content>{{ content }}</my-content>
+      </div>
+    `
   }
-})
-console.log(app)
-console.log(app2 === app)
-const vm = app.mount('#app')
-console.log(vm)
-/**
- * 根组件的本质是一个对象
- * createApp执行的时候需要一个根组件 createApp(App/{})
- * 根组件是Vue的渲染起点
- * 
- * 根元素是一个HTML元素
- * createApp执行创建Vue应用实例时，需要一个HTML根元素
- * mount方法返回的是根组件实例
-*/
